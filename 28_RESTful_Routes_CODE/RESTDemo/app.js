8da3472012bdc7faefe38d860073c95be225640e
Pradyumna Kombethota Ramgopal <pradyumnahebbar552@gmail.com>
2024-06-23 09:55:22 -0400

    mangoose content updated

--- 28_RESTful_Routes_CODE/RESTDemo/app.js
+++ 28_RESTful_Routes_CODE/RESTDemo/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const { v4: uuid } = require('uuid');
+var methodOverride = require('method-override')
 
+app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: true }))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-const comments = [
+let comments = [
     {
         id: uuid(),
         username: 'Todd',
@@ -51,7 +53,7 @@ app.get('/comments/:id', (req, res) => {
     res.render('comments/show', { comment });
 });
 
-app.patch('comments/:id', (req, res) => {
+app.patch('/comments/:id', (req, res) => {
     const { id } = req.params;
     const { update } = req.body.comment;
     const foundComment = comments.find(c => c.id === id);
@@ -64,6 +66,12 @@ app.get('/comments/:id/edit', (req, res) => {
     const { id } = req.params;
     const comment = comments.find(c => c.id === id);
     res.render('comments/edit', { comment })
+})
+
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments');
 })
 
 app.get('/tacos', (req, res) => {
