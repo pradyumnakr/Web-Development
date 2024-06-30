d114cfecb7ff757777155a610c67049e81ab5da6
Pradyumna Kombethota Ramgopal <pradyumnahebbar552@gmail.com>
2024-06-30 09:06:08 -0400

    Yelpcamp content updated

--- 35_YelpCamp/schemas.js
+++ 35_YelpCamp/schemas.js
@@ -9,3 +9,13 @@ module.exports.campgroundSchema = Joi.object({
         description: Joi.string().required()
     }).required()
 });
+
+// module.exports.campgroundSchema = Joi.object({
+//     campground: Joi.object({
+//         title: Joi.string().required(),
+//         price: Joi.number().required().min(1),
+//         image: Joi.string().required(),
+//         location: Joi.string().required(),
+//         description: Joi.string().required()
+//     }).required()
+// });
